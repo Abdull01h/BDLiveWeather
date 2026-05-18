2bac0392181fa0aac9be88069bf2fb029aa6cd1c
P1L3S3I5N3E7 <mdabdull01h@gmail.com>
2026-05-18 22:01:46 +0600

    Update API_KEY, BOT_TOKEN, and CHAT_ID in config.py
    Replaced environment variable placeholders with actual values.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_KEY = os.getenv("WEATHER_API_KEY")
+API_KEY = os.getenv("3866654071f91f426cb0c9f611c95fdd")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8951335220:AAEn13rJjSsfmva70ceJC8s0Xe1miC8G9iQ")
 
-CHAT_ID = os.getenv("CHAT_ID")
+CHAT_ID = os.getenv("8472371987")
 
 REFRESH_TIME = 10
 
